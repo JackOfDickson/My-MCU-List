7354ccf2072480dd4481f10381cf3bf069f930fa
Jack Dickson <98648241+JackOfDickson@users.noreply.github.com>
2022-07-24 16:24:29 +0100

    wrote code to access api, requires url

--- src/containers/MarvelBox.js
+++ src/containers/MarvelBox.js
@@ -2,8 +2,25 @@ import React, {useState, useEffect} from "react";
 
 const MarvelBox = () => {
 
+    const [marvelFilms, setMarvelFilms] = useState([])
+    const [marvelShows, setMarvelShows] = useState([])
 
+    const useEffect(() => {
+        getMarvelFilms()
+        getMarvelShows()
+    }, [])
 
+    const getMarvelFilms = function() {
+        fetch()
+        .then(response => response.json())
+        .then(films => setMarvelFilms(films))
+    }
+
+    // const getMarvelShows = function() {
+    //     fetch()
+    //     .then(response => response.json())
+    //     .then(shows => setMarvelShows(shows))
+    // }
 
 return (
     <div>
